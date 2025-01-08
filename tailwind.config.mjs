695bb8c945f7966bb576b0bbb7049fbb376f3c35
Adrián Pisabarro García <adrian.pisabarro.garcia@gmail.com>
2025-01-08 12:57:09 +0100

    Initial commit  🚀

---
+++ tailwind.config.mjs
@@ -0,0 +1,17 @@
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	darkMode: 'class',
+	theme: {
+		extend: {
+			colors: {
+				'custom-red': '#9C3E45',
+				'custom-blue': '#135476',
+				'custom-yellow': '#E2A84D',
+				'custom-orange': '#b5551e',
+				'custom-green': '#3d6772',
+				'custom-blue-more': '#203B43',
+			}
+		},
+	},
+	plugins: [],
+}
