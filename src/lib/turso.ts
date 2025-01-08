4bd5caefa3e6b10f76d936eb9cfa7d88676583fe
Adrián Pisabarro García <adrian.pisabarro.garcia@gmail.com>
2025-01-09 00:41:25 +0100

    uuid integration ✅

--- src/lib/turso.ts
+++ src/lib/turso.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@libsql/client/web";
 
-export const turso = createClient({
+export const db = createClient({
     url: import.meta.env.TURSO_DATABASE_URL,
     authToken: import.meta.env.TURSO_AUTH_TOKEN,
 });
